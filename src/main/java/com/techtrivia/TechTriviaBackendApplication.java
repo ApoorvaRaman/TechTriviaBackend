afbb6156a972f0b1ae7e17cc197268dc07bc9d44
Apoorva Raman <apoorvaramans@gmail.com>
2025-10-04 15:54:27 +0530

    Uploaded src/

---
+++ src/main/java/com/techtrivia/TechTriviaBackendApplication.java
@@ -0,0 +1,12 @@
+package com.techtrivia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTriviaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechTriviaBackendApplication.class, args);
+    }
+}
---
+++ src/test/java/com/techtrivia/TechTriviaBackend/TechTriviaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techtrivia.TechTriviaBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechTriviaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
